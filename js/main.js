33f093545337c726e07b216e846f6727ca1a8c42
Fabrizio <whoet.wo@gmail.com>
2020-11-09 15:28:30 +0100

    Secondo esercizio completato

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//Primo esercizio
+
 var es1 = document.getElementById('one');
 var es2 = document.getElementById('two');
 var es3 = document.getElementById('three');
@@ -6,8 +8,33 @@ var studentObj = {
    'nome': 'Fabrizio',
    'cognome': 'Nicolosi',
    'età': 30
-}
+};
 
 for (var i in studentObj){
    es1.innerHTML += ' ' + studentObj[i];
+}
+
+//Secondo esercizio
+
+var studentArray = [
+   {
+      'nome': 'Fabrizio',
+      'cognome': 'Nicolosi',
+      'età': 30
+   },
+   {
+      'nome': 'Luisa',
+      'cognome': 'Luisella',
+      'età': 16
+   },
+   {
+      'nome': 'Martino',
+      'cognome': 'Martinello',
+      'età': 24
+   }
+];
+
+for(var i = 0; i < studentArray.length; i++){
+   es2.innerHTML += ' ' + studentArray[i].nome;
+   es2.innerHTML += ' ' + studentArray[i].cognome;
 }
